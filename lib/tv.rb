36f9f5c48754888d1b4cfecda714b954e2fa57f6
Carlos Villela <cv@lixo.org>
2012-10-19 23:53:04 -0300

    first shitty (but somewhat useful version) alive

--- lib/tv.rb
+++ lib/tv.rb
@@ -1,4 +1,54 @@
-require "tv/version"
+require 'tv/version'
+
+require 'yaml'
+require 'faraday'
 
 module TV
+
+  class << self
+
+    include RSpec::Matchers
+
+    def play(file)
+      raise "Could not find #{file}" unless File.exists?(file)
+      interactions = YAML.load(File.read(file))['http_interactions']
+
+      interactions.each do |interaction|
+        request = interaction['request']
+        response = interaction['response']
+
+        connection = Faraday.new do |f|
+          f.request  :url_encoded             # form-encode POST params
+          f.response :logger                  # log requests to STDOUT
+          f.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+        end
+
+        actual = connection.send(request['method'].to_sym) do |method|
+          method.url       request['uri']
+          method.body =    request['body']
+          method.headers = request['headers']
+        end
+
+        match(response, actual)
+      end
+    end
+
+    private
+
+    def match(expected, actual)
+      expected['headers'].each do |k, v|
+        case k
+        when 'Date'
+        else
+          this = [actual.headers[k]].flatten
+          that = [v].flatten
+
+          this.should(eq(that), "Header '#{k}' did not match.\n  Expected: #{that}\n       Got: #{this}")
+        end
+      end
+
+      actual.body.should == expected['body']
+    end
+
+  end
 end
--- spec/tv_spec.rb
+++ spec/tv_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
 describe TV do
+
+  Dir[File.join(File.dirname(__FILE__), 'fixtures/**/*.yml')].each do |file|
+
+    let File.basename(file, '.yml').to_sym do
+      file
+    end
+
+    it "plays back a #{File.basename(file, '.yml').gsub /_/, ' '}" do
+      TV.play(file)
+    end
+
+  end
+
 end
